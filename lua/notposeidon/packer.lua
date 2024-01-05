36c810efcb2659288a555ee99bee60ef6b92f3dc
martin <martin@techmdw.com>
2024-01-05 15:53:34 +0100

    packer udpate

--- lua/notposeidon/packer.lua
+++ lua/notposeidon/packer.lua
@@ -17,6 +17,7 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/playground')
   use 'nvim-lua/plenary.nvim'
+  use 'folke/todo-comments.nvim'
   use 'mbbill/undotree'
   use 'tpope/vim-fugitive'
   use 'github/copilot.vim'
