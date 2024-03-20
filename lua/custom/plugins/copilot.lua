63f36461171d322db276ad308d231adff1e24bb3
martin <martin@techmdw.com>
2024-03-20 10:50:52 +0100

    backup

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,13 @@
 return {
-  'github/copilot.vim',
+	"zbirenbaum/copilot.vim",
+	cmd = "Copilot",
+	build = ":Copilot auth",
+	opt = {
+		suggestion = { enable = true },
+		panel = { enable = false },
+		filetypes = {
+			markdown = true,
+			help = true,
+		},
+	},
 }
